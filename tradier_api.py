aac48d97c892fdcfd2e5775f67f15968d2724351
mason hagan <mason24h@gmail.com>
2023-12-01 11:33:34 -0500

    added functionality for tradier api access

--- tradier_api.py
+++ tradier_api.py
@@ -46,25 +46,37 @@ class ApiBridge:
         return json_response
 
     def liquidate(self):
-        pass
-        #get portfolio, sell all of it.
+        positions = bridge.get_positions()['positions']
+        if positions == 'null':
+            print('there are not positions to liquidate.')
+            return False
+        for pos in positions:
+            for p in positions[pos]:
+                ticker = p['symbol']
+                quantity = p['quantity']
+                success = (self.market_order('sell', ticker, quantity)['order']['status'] == 'ok')
+                if not success:
+                    print('liquidate() ERROR: ' + quantity + ' shares of ' + ticker + ' have not been sold due to an unsuccessful market order.')
+                    return False
+        if self.get_positions()['positions'] == None: return True
+        print('liquidate() ERROR: not all positions have been liquidated for some reason.')
+        return False
 
     def limit_order(self, ticker, quantity, price):
-        response = requests.get('https://sandbox.tradier.com/v1/accounts/'+self.account_number+'/orders',
+        response = requests.post('https://sandbox.tradier.com/v1/accounts/'+self.account_number+'/orders',
             data={'class': 'equity', 'symbol': ticker, 'side': 'buy', 'quantity': quantity, 'type': 'limit', 'duration': 'day', 'price': price},
             headers={'Authorization': 'Bearer '+self.token, 'Accept': 'application/json'}
         )
         json_response = response.json()
         return json_response
 
-    def market_order(self, ticker, quantity):
-        response = requests.get('https://sandbox.tradier.com/v1/accounts/'+self.account_number+'/orders',
-            data={'class': 'equity', 'symbol': ticker, 'side': 'buy', 'quantity': quantity, 'type': 'market', 'duration': 'day'},
+    def market_order(self, side, ticker, quantity):
+        response = requests.post('https://sandbox.tradier.com/v1/accounts/'+self.account_number+'/orders',
+            data={'class': 'equity', 'symbol': ticker, 'side': side, 'quantity': quantity, 'type': 'market', 'duration': 'day'},
             headers={'Authorization': 'Bearer '+self.token, 'Accept': 'application/json'}
         )
         json_response = response.json()
-        print(response.status_code)
-        print(json_response)
+        return json_response
 
 
 
@@ -72,9 +84,10 @@ bridge = ApiBridge('LUiHZ2fsw2f8lfptk0UTmLH5BnrX','VA43237255')
 
 #print(bridge.get_balance())
 
-#print(bridge.limit_order('AAPL', 30, '189'))
-bridge.market_order('AMZN', 10)
-
+#print(bridge.limit_order('AAPL', 30, '191'))
+#bridge.market_order('AMZN', 10)
+#positions = bridge.get_positions()['positions']
+print(bridge.liquidate())
 
 #def even_investment(tickers):
 
